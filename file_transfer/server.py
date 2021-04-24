cae413cc992f60b8f386ed336b6da80f927fa024
ahaguilar <hiram1121@gmail.com>
2021-04-23 23:46:45 -0600

    working

---
+++ file_transfer/server.py
@@ -0,0 +1,82 @@
+import os, socket, sys
+
+sys.path.append("../lib")
+import params
+from framedSock import framedReceive
+
+PATH = ".Receive"
+HOST = "127.0.0.1"
+
+def server():
+    switchesVarDefaults = (
+        (('1', '--listenPort'), 'listenPort', 50001),
+        (('?', '--usage'), 'usage', False),
+        (('d', '--debug'), 'debug', False),
+    )
+
+    parameterMap = params.parseParams(switchesVarDefaults)
+    listenPort, debug = parameterMap['listenPort'], parameterMap['debug']
+
+    if parameterMap['usage']:
+        params.usage()
+
+    bindAddress = (HOST, listenPort)
+
+    # listening socket created
+    listenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listenSocket.bind(bindAddress)
+
+    listenSocket.listen(10)
+    print("Listening on: ", bindAddress)
+
+    # check if files arebeing received, if not found then create it
+    if not os.path.exists(PATH):
+        os.makedirs(PATH)
+    os.chdir(PATH)
+    while 1:
+        connection, address = listenSocket.accept()
+
+        if not connection or not address:
+            sys.exit(1)
+
+        if not os.fork():
+            print("connect by: ", address)
+
+            try:
+                fileName, contents = framedReceive(connection, debug)
+            except:
+                print("Error: File transfer was not successful!")
+                connection.send(str(0).encode())
+                sys.exit(1)
+
+            fileName = fileName.decode()
+            writeFile(connection, address, fileName, contents)
+
+            connection.send(str(1).encode())
+            sys.exit(0)
+
+def writeFile(connection, address, fileName, contents):
+
+    if connection is None:
+        raise TypeError
+    if address is None:
+        raise TypeError
+    if fileName is None:
+        raise TypeError
+    if contents is None:
+        raise TypeError
+
+    try:
+        #create a file and write with binary
+        writer = open(fileName, 'w+b')
+        writer.write(contents)
+        writer.close()
+
+        print("File %s received from %s" % (fileName, address))
+    except FileNotFoundError:
+        print("File Not Found Error: File %s not found" % fileName)
+        connection.send(str(0).encode())
+        sys.exit(1)
+
+if __name__ == "__main__":
+    server()
